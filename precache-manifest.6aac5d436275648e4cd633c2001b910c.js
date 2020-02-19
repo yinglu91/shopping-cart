644eff5d4ac0dc7bac6c7742af4780aaa2ba0747
Ying Lu <yinglu91@gmail.com>
2020-02-19 00:03:27 -0500

    Updates

---
+++ precache-manifest.6aac5d436275648e4cd633c2001b910c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c54ff0e29738099536b0f58f4bdebd3",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "22722869c3a34c753329",
+    "url": "/shopping-cart/static/css/main.8b281e51.chunk.css"
+  },
+  {
+    "revision": "23b9f78c27455f419d59",
+    "url": "/shopping-cart/static/js/2.f8d6aec7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/shopping-cart/static/js/2.f8d6aec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22722869c3a34c753329",
+    "url": "/shopping-cart/static/js/main.07c13d0c.chunk.js"
+  },
+  {
+    "revision": "4937b591c9ce41d765ca",
+    "url": "/shopping-cart/static/js/runtime-main.199c6bd1.js"
+  }
+]);
